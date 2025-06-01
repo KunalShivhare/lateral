5cf64e6aa0a9fb85a11a9cdfccf649ae5bbdcf47
Kunal Shivhare <KunalShivhare58@gmail.com>
2025-06-02 00:32:52 +0530

    fix:PaymentPlan as per theme

--- src/components/dashboard/PaymentPlan.tsx
+++ src/components/dashboard/PaymentPlan.tsx
@@ -1,14 +1,20 @@
 export function PaymentPlan() {
   return (
-    <div className="bg-[var(--cardBackground)] border-[1px] border-[#3F3F3F] rounded-lg mt-6 overflow-hidden p-4">
+    <div className="bg-[var(--cardBackground)] border-[1px] border-[var(--border)] rounded-lg mt-6 overflow-hidden p-4">
       <div className="flex items-center justify-between">
         <div>
-          <h3 className="text-[#939292] text-sm mb-1">Payment plan</h3>
-          <div className="text-white font-medium">$344.00</div>
+          <h3 className="text-[var(--primary-text)] text-sm mb-1">
+            Payment plan
+          </h3>
+          <div className="text-[var(--primary-text)] font-medium">$344.00</div>
         </div>
         <div>
-          <h3 className="text-[#939292] text-sm mb-1">Next payment due</h3>
-          <div className="text-white font-medium">June 15, 2020</div>
+          <h3 className="text-[var(--primary-text)] text-sm mb-1">
+            Next payment due
+          </h3>
+          <div className="text-[var(--primary-text)] font-medium">
+            June 15, 2020
+          </div>
         </div>
         <div>
           <button className="text-[#058FFF] text-sm">
