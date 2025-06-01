e96441efae06810d10a0bbcb9283ad41cecaf9ee
Kunal Shivhare <KunalShivhare58@gmail.com>
2025-06-02 00:31:54 +0530

    fix:Linked records as per theme

--- src/components/dashboard/LinkedRecords.tsx
+++ src/components/dashboard/LinkedRecords.tsx
@@ -1,14 +1,18 @@
 export function LinkedRecords() {
   return (
-    <div className="bg-[var(--cardBackground)] border-[1px] border-[#3F3F3F] rounded-lg mt-6 overflow-hidden p-4">
+    <div className="bg-[var(--cardBackground)] border-[1px] border-[var(--border)] rounded-lg mt-6 overflow-hidden p-4">
       <div className="flex items-center justify-between">
         <div className="flex items-center">
-          <h3 className="text-white text-base font-medium">Linked Records</h3>
-          <span className="text-[#939292] text-base pl-4 ">3</span>
+          <h3 className="text-[var(--primary-text)] text-base font-medium">
+            Linked Records
+          </h3>
+          <span className="text-[var(--primary-text)] text-base pl-4 ">3</span>
         </div>
         <div className="flex">
-          <h3 className="text-[#939292] text-xs">Amount linked:</h3>
-          <span className="text-xs pl-1">$34,567.00</span>
+          <h3 className="text-[var(--primary-text)] text-xs">Amount linked:</h3>
+          <span className="text-xs pl-1 text-[var(--primary-text)]">
+            $34,567.00
+          </span>
         </div>
         <div className="flex justify-end ">
           <button className="text-[#058FFF] text-sm">See Linked Records</button>
